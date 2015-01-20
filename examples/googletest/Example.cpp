442ba15916160f41bc1b5369944db9b6b82ec2db
offa <bm-dev@yandex.com>
2015-01-20 15:28:59 +0100

    Example added.

---
+++ examples/googletest/Example.cpp
@@ -0,0 +1,37 @@
+
+#include <gtest/gtest.h>
+
+TEST(Example, testThatWorks)
+{
+    int i = 3;
+    EXPECT_EQ(3, i);
+}
+
+TEST(Example, testThatWorks2)
+{
+    double d = 3.57;
+    
+    EXPECT_DOUBLE_EQ(3.57, d);
+}
+
+TEST(Example, testThatTakesSomeTime)
+{
+    unsigned long value = 0;
+    
+    for( unsigned long i=0; i<10000000; i++ )
+    {
+        value += i;
+    }
+    
+    EXPECT_TRUE(value != 0UL);
+}
+
+/*
+ * This test will fail!
+ */
+TEST(Example, testThatFails)
+{
+    int i = 99979;
+    
+    EXPECT_EQ(99999, i);
+}
