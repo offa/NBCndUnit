8e1c28a0ea42ecebe9ae84f6ffd7cc7f51754234
offa <bm-dev@yandex.com>
2015-01-22 16:36:22 +0100

    Comments added.

--- examples/cpputest/AllTests.cpp
+++ examples/cpputest/AllTests.cpp
@@ -5,8 +5,8 @@
 int main(int argc, char** argv)
 {
     std::vector<const char*> args(argv, argv + argc);
-    args.push_back("-v");
-    args.push_back("-c");
+    args.push_back("-v"); // Enable verbose output
+    args.push_back("-c"); // Enable colored output (optional)
     
     return CommandLineTestRunner::RunAllTests(args.size(), &args[0]);
 }
