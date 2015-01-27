891172a66ee6bc8fff6b0b4ba4dd2f873a8ff445
offa <bm-dev@yandex.com>
2015-01-27 11:59:25 +0100

    Documentation added and some minor changes.

--- src/main/java/bv/offa/netbeans/cnd/unittest/Installer.java
+++ src/main/java/bv/offa/netbeans/cnd/unittest/Installer.java
@@ -1,3 +1,23 @@
+/*
+ * NBCndUnit - C/C++ unit tests for NetBeans.
+ * Copyright (C) 2015  offa
+ * 
+ * This file is part of NBCndUnit.
+ *
+ * NBCndUnit is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * NBCndUnit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with NBCndUnit.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package bv.offa.netbeans.cnd.unittest;
 
 import java.lang.reflect.Field;
@@ -8,6 +28,12 @@ import org.openide.modules.ModuleInfo;
 import org.openide.modules.ModuleInstall;
 import org.openide.modules.Modules;
 
+/**
+ * The class {@code Installer} implements an installer which will add this
+ * module as friend to other modules.
+ * 
+ * @author  offa
+ */
 public class Installer extends ModuleInstall
 {
     private static final long serialVersionUID = 1L;
@@ -23,6 +49,14 @@ public class Installer extends ModuleInstall
 
     
     
+    /**
+     * Called if the module is loaded. It adds this module to the friend-list of
+     * specified modules. If there are no modules specified, this method
+     * does nothing.
+     * 
+     * @throws IllegalStateException    Thrown if an exception occurs
+     *                                  while loading 
+     */
     @Override
     public void validate() throws IllegalStateException
     {
@@ -33,6 +67,12 @@ public class Installer extends ModuleInstall
     }
     
     
+    /**
+     * Adds additional friends to specified modules. The modules are specified
+     * by their module code name base within {@code targetModules}.
+     * 
+     * @exception IllegalStateException     Will rethrow previous exceptions
+     */
     private void addFriends()
     {
         try
@@ -53,12 +93,15 @@ public class Installer extends ModuleInstall
                 
                 final ModuleInfo moduleInfo = (ModuleInfo) dependency;
                 
-                final Method dataMethod = Class.forName("org.netbeans.Module", true, moduleInfo.getClass().getClassLoader()).getDeclaredMethod("data");
+                final Class<?> moduleClass = Class.forName("org.netbeans.Module", true, moduleInfo.getClass().getClassLoader());
+                final Method dataMethod = moduleClass.getDeclaredMethod("data");
                 dataMethod.setAccessible(true);
                 
                 final Object dataValue = dataMethod.invoke(moduleInfo);
                 
-                final Field friendNamesField = Class.forName("org.netbeans.ModuleData", true, dataValue.getClass().getClassLoader()).getDeclaredField("friendNames");
+                final Class<?> moduleDataClass = Class.forName("org.netbeans.ModuleData", true, dataValue.getClass().getClassLoader());
+                final Field friendNamesField = moduleDataClass.getDeclaredField("friendNames");
+                
                 updateFriendsValue(friendNamesField, dataValue, codeNameBase);
             }
         }
@@ -77,17 +120,27 @@ public class Installer extends ModuleInstall
     }
     
     
-    private void updateFriendsValue(Field field, Object obj, String friendToAdd) throws IllegalArgumentException, IllegalAccessException
+    /**
+     * Adds the module {@code friendToAdd} to the {@code friendField} of
+     * object {@code obj}.
+     * 
+     * @param friendField   Field of the friendlist
+     * @param obj           Object to modify
+     * @param friendToAdd   Module code name base to add as friend
+     * @throws IllegalAccessException   On failure of reflection access
+     */
+    private void updateFriendsValue(Field friendField, Object obj, String friendToAdd)
+            throws IllegalAccessException
     {
-        field.setAccessible(true);
+        friendField.setAccessible(true);
         
         @SuppressWarnings("unchecked")
-        Set<String> value = (Set<String>) field.get(obj);
+        Set<String> value = (Set<String>) friendField.get(obj);
         assert(value != null);
         
         // TODO: Test for null and add then!?
         Set<String> newValue = new HashSet<String>(value);
         newValue.add(friendToAdd);
-        field.set(obj, newValue);
+        friendField.set(obj, newValue);
     }
 }
