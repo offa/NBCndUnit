b042533ff94867cbed2dccbc7d196ac0f3dace0c
offa <bm-dev@yandex.com>
2015-03-18 13:10:15 +0100

    Fixtures, setup and teardown added for libunittestc++.

--- src/main/resources/bv/offa/netbeans/cnd/unittest/templates/LibunittestCppTestSuite.cpp
+++ src/main/resources/bv/offa/netbeans/cnd/unittest/templates/LibunittestCppTestSuite.cpp
@@ -9,11 +9,29 @@
 
 using namespace unittest::assertions;
 
+struct ${suiteName}Fixture
+{
+<#if generateSetup == true >
+    ${suiteName}Fixture()
+    {
+        // Setup ...
+    }
+
+</#if>
+<#if generateTeardown == true >
+    virtual ~${suiteName}Fixture() noexcept(false)
+    {
+        // Teardown ...
+    }
+
+</#if>
+};
+
 COLLECTION(${suiteName})
 {
 <#if generateTestCases == true >
 <#list testCases as testCase>
-    TEST(${testCase})
+    TEST_FIXTURE(${suiteName}Fixture, ${testCase})
     {
         assert_true(false, "Nothing to test yet ...", SPOT);
     }
