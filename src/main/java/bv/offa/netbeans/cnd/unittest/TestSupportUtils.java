aab52f8650a19cf9d2b5d71b8b22e03c75d7fc2c
offa <bm-dev@yandex.com>
2015-06-18 19:24:44 +0200

    Cleanup.

--- src/main/java/bv/offa/netbeans/cnd/unittest/TestSupportUtils.java
+++ src/main/java/bv/offa/netbeans/cnd/unittest/TestSupportUtils.java
@@ -25,7 +25,6 @@ import bv.offa.netbeans.cnd.unittest.api.CndTestSuite;
 import bv.offa.netbeans.cnd.unittest.ui.TestRunnerUINodeFactory;
 import java.lang.reflect.Field;
 import java.util.concurrent.Callable;
-import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.netbeans.api.project.Project;
