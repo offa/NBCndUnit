cdea61b8a9b39b1f77616cee767f6d59bc7e9175
offa <bm-dev@yandex.com>
2015-08-08 16:14:19 +0200

    Typo fixed.

--- examples/cpputest/AllTests.cpp
+++ examples/cpputest/AllTests.cpp
@@ -6,7 +6,7 @@ int main(int argc, char** argv)
 {
     std::vector<const char*> args(argv, argv + argc);
     args.push_back("-v"); // Verbose output (mandatory!)
-    args.push_back("-c"); // Colored outupt (optional)
+    args.push_back("-c"); // Colored output (optional)
 
     return CommandLineTestRunner::RunAllTests(args.size(), &args[0]);
 }
