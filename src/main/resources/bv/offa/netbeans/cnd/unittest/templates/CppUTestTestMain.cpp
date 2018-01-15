17a7f3a004b22f57a0f8bab2ab26f3c7773fa939
offa <bm-dev@yandex.com>
2018-01-15 15:53:04 +0100

    Test main templates updated.

--- src/main/resources/bv/offa/netbeans/cnd/unittest/templates/CppUTestTestMain.cpp
+++ src/main/resources/bv/offa/netbeans/cnd/unittest/templates/CppUTestTestMain.cpp
@@ -1,4 +1,4 @@
-/* 
+/*
  * File:   ${nameAndExt}
  * Author: ${user}
  *
@@ -11,10 +11,22 @@
 int main(int argc, char** argv)
 {
     std::vector<const char*> args(argv, argv + argc);
+<#if enableModernCpp == true >
+    args.emplace_back("-v"); // Verbose output (mandatory!)
+<#else>
     args.push_back("-v"); // Verbose output (mandatory!)
-    <#if enableColor == true >
+</#if>
+<#if enableColor == true >
+    <#if enableModernCpp == true >
+    args.emplace_back("-c"); // Colored output (optional)
+    <#else>
     args.push_back("-c"); // Colored output (optional)
     </#if>
-    
+</#if>
+
+<#if enableColor == true >
+    return RUN_ALL_TESTS(args.size(), args.data());
+<#else>
     return RUN_ALL_TESTS(args.size(), &args[0]);
+</#if>
 }
--- src/main/resources/bv/offa/netbeans/cnd/unittest/templates/LibunittestCppTestMain.cpp
+++ src/main/resources/bv/offa/netbeans/cnd/unittest/templates/LibunittestCppTestMain.cpp
@@ -1,4 +1,4 @@
-/* 
+/*
  * File:   ${nameAndExt}
  * Author: ${user}
  *
@@ -11,7 +11,15 @@
 int main(int argc, char** argv)
 {
     std::vector<const char*> args(argv, argv + argc);
+<#if enableModernCpp == true >
+    args.emplace_back("-v"); // Verbose output (mandatory!)
+<#else>
     args.push_back("-v"); // Verbose output (mandatory!)
+</#if>
 
+<#if enableModernCpp == true >
+    return unittest::process(args.size(), const_cast<char**>(args.data()));
+<#else>
     return unittest::process(args.size(), const_cast<char**>(&args[0]));
+</#if>
 }
