a1f721ea93023d784b4eadd4352e9a10e92f6452
offa <bm-dev@yandex.com>
2018-01-15 19:51:36 +0100

    Workaround documented.

--- script/ci_build.sh
+++ script/ci_build.sh
@@ -2,9 +2,11 @@
 
 set -ex
 
+# Workaround for broken JDK9 (see #48)
 if java -fullversion 2>&1 | grep -q '"9.';
 then
-    wget https://circle-downloads.s3.amazonaws.com/circleci-images/cache/linux-amd64/openjdk-9-slim-cacerts -O /etc/ssl/certs/java/cacerts
+    wget https://circle-downloads.s3.amazonaws.com/circleci-images/cache/linux-amd64/openjdk-9-slim-cacerts \
+        -O /etc/ssl/certs/java/cacerts
 fi
 
 
