acd718d2dd74e28b45ee93e39c9569c5e153f5bd
offa <bm-dev@yandex.com>
2018-02-04 18:05:58 +0100

    Workaround for broken JDK9 (#48) removed as this is included in the docker image now.

--- script/ci_build.sh
+++ script/ci_build.sh
@@ -2,14 +2,6 @@
 
 set -ex
 
-# Workaround for broken JDK9 (see #48)
-if java -fullversion 2>&1 | grep -q '"9.';
-then
-    wget https://circle-downloads.s3.amazonaws.com/circleci-images/cache/linux-amd64/openjdk-9-slim-cacerts \
-        -O /etc/ssl/certs/java/cacerts
-fi
-
-
 # Install Dependencies
 mvn install -DskipTests=true -Dmaven.javadoc.skip=true -B -V
 
