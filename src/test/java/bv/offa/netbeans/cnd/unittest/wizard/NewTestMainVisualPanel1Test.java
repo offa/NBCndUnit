72f4a15d884ff53a0cd9c4e571065eb24f8a01b1
offa <bm-dev@yandex.com>
2018-03-24 18:01:05 +0100

    Missing header and JUnit Tag added, cleanup and imports fixed.

--- src/test/java/bv/offa/netbeans/cnd/unittest/wizard/NewTestMainVisualPanel1Test.java
+++ src/test/java/bv/offa/netbeans/cnd/unittest/wizard/NewTestMainVisualPanel1Test.java
@@ -1,44 +1,36 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * NBCndUnit - C/C++ unit tests for NetBeans.
+ * Copyright (C) 2015-2018  offa
+ *
+ * This file is part of NBCndUnit.
+ *
+ * NBCndUnit is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * NBCndUnit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with NBCndUnit.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package bv.offa.netbeans.cnd.unittest.wizard;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.jupiter.api.Tag;
 
-
+@Tag("ui")
 public class NewTestMainVisualPanel1Test
 {
     private final NewTestMainVisualPanel1 panel = new NewTestMainVisualPanel1();
 
-    @BeforeClass
-    public static void setUpClass()
-    {
-    }
 
-    @AfterClass
-    public static void tearDownClass()
-    {
-    }
-
-    @Before
-    public void setUp()
-    {
-    }
-
-    @After
-    public void tearDown()
-    {
-    }
-
-
-    @org.junit.jupiter.api.Test
+    @Test
     public void defaultSettings()
     {
         assertTrue(panel.getEnableVerbose());
